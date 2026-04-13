95c6911b5e525365d4e1fc51ac3536f6feb03373
gordonbchen <gordonbchen@gmail.com>
2026-04-12 20:29:57 -0400

    start async impala

--- async_single_machine.py
+++ async_single_machine.py
@@ -1,14 +1,16 @@
 """IMPALA: https://arxiv.org/pdf/1802.01561."""
+import multiprocessing as mp
+from multiprocessing.synchronize import Event as EventClass
 from dataclasses import dataclass
+from queue import Full, Empty
 import envpool
 import torch
 import numpy as np
-from cli_params import CLIParams
 from torch import Tensor
 from torch.distributions import Categorical
 from torch.optim import Adam
 from torch.utils.tensorboard import SummaryWriter
-
+from cli_params import CLIParams
 from agent import Agent
 from env import EnvPoolEpisodeStats
 
@@ -17,13 +19,21 @@ torch.set_float32_matmul_precision("high")
 
 @dataclass
 class HyperParams(CLIParams):
-    n_epochs: int = 4000
-    n_steps: int = 128
-    n_envs: int = 16
-    device: str = "cuda"
+    # Actor.
+    n_actors: int = 5
+    rollout_queue_size: int = 64
+    n_rollout_steps: int = 64
+    policy_sync_freq: int = 4
 
-    lr: float = 3e-4
+    # Env.
+    n_envs: int = 16
+    n_frame_stack: int = 4
+    env_name: str = "Pong-v5"
+
+    # Training.
+    n_epochs: int = 4000
     update_steps: int = 4
+    lr: float = 3e-4
 
     discount_gamma: float = 0.99
     impala_max_rho: float = 1.0
@@ -32,16 +42,76 @@ class HyperParams(CLIParams):
     entropy_coeff: float = 0.01
     max_grad_norm: float = 0.5
 
-    n_frame_stack: int = 4
-
+    # Meta.
+    device: str = "cuda"
     output_dir: str = "runs/test"
+
+
+def actor_func(actor_id: int, HP: HyperParams, rollout_queue: mp.Queue, weight_queue: mp.Queue, stop_event: EventClass):
+    print(f"HELLO from actor {actor_id}.")
+
+    # Create envs.
+    envs = envpool.make_gymnasium(HP.env_name, num_envs=HP.n_envs)
+    envs = EnvPoolEpisodeStats(envs, n_envs=HP.n_envs)
+
+    # Create agent.
+    agent = Agent(HP.n_frame_stack, envs.action_space.n)
+    policy_version = -1
+
+    # Storage buffers.
+    OBS_SHAPE = (HP.n_frame_stack, 84, 84)
+    obss = torch.zeros((HP.n_rollout_steps+1, HP.n_envs) + OBS_SHAPE, dtype=torch.float32, device=HP.device)
+    dones = torch.zeros((HP.n_rollout_steps+1, HP.n_envs), dtype=torch.float32, device=HP.device)
+
+    actions = np.zeros((HP.n_rollout_steps, HP.n_envs), dtype=np.int64)
+    rewards = np.zeros((HP.n_rollout_steps, HP.n_envs), dtype=np.float32)
+    old_log_probs = np.zeros((HP.n_rollout_steps, HP.n_envs), dtype=np.float32)
+
+    # Env init.
+    obs, _ = envs.reset()
+    obs = torch.tensor(obs, dtype=torch.float32, device=HP.device)
+    done = torch.zeros(HP.n_envs, dtype=torch.float32, device=HP.device)
+
+    i = 0
+    while not stop_event.is_set():
+        if i % HP.policy_sync_freq == 0:
+            while not stop_event.is_set():
+                try:
+                    learner_policy_version, state_dict = weight_queue.get(timeout=0.1)
+                    break
+                except Empty:
+                    continue
+            if stop_event.is_set():
+                break
+
+            if learner_policy_version != policy_version:
+                policy_version = learner_policy_version
+                agent.load_state_dict(state_dict)
+                agent.to(device=HP.device).eval()
+
+        mean_reward, mean_episode_length, n_episodes = sample_trajectories(
+            agent, envs, HP.n_rollout_steps, obs, done, obss, dones, actions, rewards, old_log_probs
+        )
+        rollout = (actor_id, policy_version, mean_reward, mean_episode_length, n_episodes,
+                   obss.cpu().to(dtype=torch.uint8).numpy(), dones.cpu().numpy(), actions, rewards, old_log_probs)
+        while not stop_event.is_set():
+            try:
+                rollout_queue.put(rollout, timeout=0.1)
+                break
+            except Full:
+                continue
+
+        i += 1
+
+    envs.close()
+    print(f"GOODBYE from actor {actor_id}.")
 
 
 @torch.no_grad()
 def sample_trajectories(
-        agent: Agent, envs, n_steps: int, action_map: np.ndarray, obs: Tensor, done: Tensor,
-        obss: Tensor, dones: Tensor, actions: Tensor, rewards: Tensor, old_log_probs: Tensor,
-    ) -> tuple[float, int, int]:
+        agent: Agent, envs, n_rollout_steps: int, obs: Tensor, done: Tensor,
+        obss: Tensor, dones: Tensor, actions: np.ndarray, rewards: np.ndarray, old_log_probs: np.ndarray,
+    ) -> tuple[float | None, float | None, int]:
     total_reward = 0.0
     total_episode_length = 0
     n_episodes = 0
@@ -50,7 +120,7 @@ def sample_trajectories(
     obss[0] = obs
     dones[0] = done
 
-    for t in range(n_steps):
+    for t in range(n_rollout_steps):
         # import matplotlib.pyplot as plt
         # plt.imshow(obs[0][0].cpu().numpy(), cmap="gray")
         # plt.show()
@@ -58,11 +128,11 @@ def sample_trajectories(
         action_dist = Categorical(logits=logits)
         action = action_dist.sample()
 
-        actions[t] = action
-        old_log_probs[t] = action_dist.log_prob(action)
+        actions[t] = action.cpu().numpy()
+        old_log_probs[t] = action_dist.log_prob(action).cpu().numpy()
 
-        obs, reward, terminated, truncated, info = envs.step(action_map[action.cpu().numpy()])
-        rewards[t] = torch.tensor(reward, dtype=torch.float32, device=rewards.device)
+        obs, reward, terminated, truncated, info = envs.step(action.cpu().numpy())
+        rewards[t] = reward
 
         obs = torch.tensor(obs, dtype=torch.float32, device=obss.device)
         done = torch.tensor(terminated | truncated, dtype=torch.float32, device=dones.device)
@@ -75,13 +145,15 @@ def sample_trajectories(
             total_episode_length += info["stats"]["lens"][done_mask].sum()
             n_episodes += int(done_mask.sum())
 
-    return total_reward, total_episode_length, n_episodes
+    mean_reward = None if n_episodes == 0 else total_reward / n_episodes
+    mean_episode_length = None if n_episodes == 0 else total_episode_length / n_episodes
+    return mean_reward, mean_episode_length, n_episodes
 
 
 def optimize_model(
         agent: Agent, obss: Tensor, actions: Tensor, rewards: Tensor, dones: Tensor, old_log_probs: Tensor, HP: HyperParams
     ) -> tuple[float, float, float, float, float]:
-    logits, values = agent(obss.view(-1, *OBS_SHAPE))
+    logits, values = agent(obss.reshape(-1, *obss.shape[2:]))
     logits, values = [t.view(-1, HP.n_envs, *t.shape[1:]) for t in (logits, values)]
     action_dist = Categorical(logits=logits[:-1])
     log_probs = action_dist.log_prob(actions)
@@ -112,7 +184,7 @@ def calc_vtrace_targets(
     c = action_ratios.clamp(max=HP.impala_max_c)
 
     vtrace_deltas = torch.zeros_like(values)
-    for t in reversed(range(HP.n_steps)):
+    for t in reversed(range(HP.n_rollout_steps)):
         vtrace_deltas[t] = rho[t] * td_deltas[t] + next_nonterminal[t] * HP.discount_gamma * c[t] * vtrace_deltas[t+1]
     vtrace_targets = vtrace_deltas + values
 
@@ -124,43 +196,49 @@ def calc_vtrace_targets(
 if __name__ == "__main__":
     HP = HyperParams()
 
-    # Create envs.
-    envs = envpool.make_gymnasium("Pong-v5", num_envs=HP.n_envs)
-    envs = EnvPoolEpisodeStats(envs, n_envs=HP.n_envs)
-    # 0 = NOOP, 1 = LEFT, 2 = RIGHT.
-    ACTION_MAP = np.array([0, 3, 2], dtype=np.int64)
+    # Create actors.
+    mp.set_start_method("spawn")
+    rollout_queue = mp.Queue(HP.rollout_queue_size)  # TODO: circular buffer?
+    stop_event = mp.Event()
+    weight_queues = [mp.Queue(1) for _ in range(HP.n_actors)]
+    actors = [
+        mp.Process(target=actor_func, args=(i, HP, rollout_queue, weight_queues[i], stop_event))
+        for i in range(HP.n_actors)
+    ]
+    for actor in actors:
+        actor.start()
 
     # Create agent.
-    agent = Agent(HP.n_frame_stack, len(ACTION_MAP)).to(device=HP.device)
-    agent.compile()
+    envs = envpool.make_gymnasium(HP.env_name, num_envs=1)  # TODO: make this exactly like actor env?
+    agent = Agent(HP.n_frame_stack, envs.action_space.n).to(device=HP.device)
     optim = Adam(agent.parameters(), lr=HP.lr, fused=True)
-
-    # Storage buffers.
-    OBS_SHAPE = (HP.n_frame_stack, 84, 84)
-    obss = torch.zeros((HP.n_steps+1, HP.n_envs) + OBS_SHAPE, dtype=torch.float32, device=HP.device)
-    dones = torch.zeros((HP.n_steps+1, HP.n_envs), dtype=torch.float32, device=HP.device)
-    actions = torch.zeros((HP.n_steps, HP.n_envs), dtype=torch.int64, device=HP.device)
-    rewards = torch.zeros((HP.n_steps, HP.n_envs), dtype=torch.float32, device=HP.device)
-    old_log_probs = torch.zeros((HP.n_steps, HP.n_envs), dtype=torch.float32, device=HP.device)
-
-    # Env init.
-    obs, _ = envs.reset()
-    obs = torch.tensor(obs, dtype=torch.float32, device=HP.device)
-    done = torch.zeros(HP.n_envs, dtype=torch.float32, device=HP.device)
+    policy_version = 0
 
     log = SummaryWriter(log_dir=HP.output_dir)
     for epoch in range(HP.n_epochs):
-        # Sample trajectories.
-        total_reward, total_episode_length, n_episodes = sample_trajectories(
-            agent, envs, HP.n_steps, ACTION_MAP, obs, done, obss, dones, actions, rewards, old_log_probs
-        )
-        mean_total_reward = None
+        # Try to give actors new weights. If prev weights not consumed, just wait.
+        new_weights = (policy_version, agent.state_dict())
+        for weight_queue in weight_queues:
+            try:
+                weight_queue.put(new_weights, block=False)
+            except Full:
+                pass
+        policy_version += 1
+
+        (actor_id, actor_policy_version, mean_reward, mean_episode_length, n_episodes,
+         obss, dones, actions, rewards, old_log_probs) = rollout_queue.get()
+        obss, dones, actions, rewards, old_log_probs = [
+            torch.tensor(t, dtype=torch.float32, device=HP.device)
+            for t in (obss, dones, actions, rewards, old_log_probs)
+        ]
+
         if n_episodes > 0:
-            mean_total_reward = total_reward / n_episodes
-            log.add_scalar("ep_stats/reward", mean_total_reward, epoch)
-            log.add_scalar("ep_stats/length", total_episode_length / n_episodes, epoch)
+            log.add_scalar("ep_stats/reward", mean_reward, epoch)
+            log.add_scalar("ep_stats/length", mean_episode_length, epoch)
         log.add_scalar("ep_stats/episodes", n_episodes, epoch)
-        print(f"{epoch}: reward={mean_total_reward}")
+        log.add_scalar("async/actor_policy_version", actor_policy_version, epoch)
+        log.add_scalar("async/rollout_queue_len", rollout_queue.qsize(), epoch)
+        print(f"{epoch}: reward={mean_reward}")
 
         # Linearly decay lr to 0.
         optim.param_groups[0]["lr"] = HP.lr - (HP.lr / HP.n_epochs) * epoch
@@ -177,5 +255,11 @@ if __name__ == "__main__":
         log.add_scalar("train/lr", optim.param_groups[0]["lr"], epoch)
         log.add_scalar("train/action_ratios", mean_action_ratio, epoch)
 
+    # Cleanup.
     log.close()
-    envs.close()
+    stop_event.set()
+    print("STOP issued, waiting for actors to terminate.")
+    for actor in actors:
+        actor.join()
+    print("DONE waiting for actors to terminate.")
+    print("GOODBYE from learner.")
